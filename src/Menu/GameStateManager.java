b2ee40a2963dce628c3390a64e88ff1fb71d472b
jchrislee16 <jchrislee16@gmail.com>
2022-02-07 01:51:53 -0500

    change
    .

--- src/Menu/GameStateManager.java
+++ src/Menu/GameStateManager.java
@@ -1,12 +1,48 @@
 package Menu;
 
 
+import java.awt.Graphics2D;
 import java.util.ArrayList;
 
 public class GameStateManager {
 	
 	
 	private ArrayList<GameState> gamestates;
+	private int currentstate;
+	
+	public static final int MENUSTATE = 0;
+	public static final int RESOLUTION = 1;
+	public static final int CONTROL = 2;
+	
+	public GameStateManager()
+	{
+		gamestates = new ArrayList<GameState>();
+		currentstate = 0;
+		gamestates.add(new MenuState());
+		
+	}
+	
+	
+	public void setState(int state)
+	{
+		currentstate = state;
+		gamestates.get(currentstate).init();
+		
+	}
+	
+	
+	public void update() 
+	{
+		gamestates.get(currentstate).update();
+	}
+	
+	public void draw(Graphics2D g)
+	{
+		gamestates.get(currentstate).draw(g);
+	}
+	
+	public void keyPressed(int k) {}
+	public void keyReleased(int k) {}
 	
 	
 	
--- src/Menu/MenuState.java
+++ src/Menu/MenuState.java
@@ -1,5 +1,43 @@
 package Menu;
 
-public class MenuState{
+import java.awt.Graphics2D;
+
+public class MenuState extends GameState{
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(int k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(int k) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(int k) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
